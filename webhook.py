c2cf9da4a89321f812ab78e0ecab847d61d651e4
mizjogit <git@mizjo.com>
2015-05-07 20:17:09 +1000

    test

--- webhook.py
+++ webhook.py
@@ -29,8 +29,6 @@ if os.environ.get('USE_PROXYFIX', None) == 'true':
 app = Flask(__name__)
 app.debug = os.environ.get('DEBUG') == 'true'
 
-# The repos.json file should be readable by the user running the Flask app,
-# and the absolute path should be given by this environment variable.
 REPOS_JSON_PATH = os.environ['FLASK_GITHUB_WEBHOOK_REPOS_JSON']
 
 
@@ -39,6 +37,7 @@ def index():
     if request.method == 'GET':
         return 'OK'
     elif request.method == 'POST':
+	print request;
         # Store the IP address of the requester
         request_ip = ipaddress.ip_address(u'{0}'.format(request.remote_addr))
 
@@ -85,8 +84,7 @@ def index():
             # Check if POST request signature is valid
             key = repo.get('key', None)
             if key:
-                signature = request.headers.get('X-Hub-Signature').split(
-                    '=')[1]
+                signature = request.headers.get('X-Hub-Signature').split('=')[1]
                 if type(key) == unicode:
                     key = key.encode()
                 mac = hmac.new(key, msg=request.data, digestmod=sha1)
