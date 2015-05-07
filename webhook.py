97ca8e4cdb3d15d073d2261eaf1f955bb327c139
mizjogit <git@mizjo.com>
2015-05-07 22:04:22 +1000

    repo and webhook fixed

--- webhook.py
+++ webhook.py
@@ -29,7 +29,10 @@ if os.environ.get('USE_PROXYFIX', None) == 'true':
 app = Flask(__name__)
 app.debug = os.environ.get('DEBUG') == 'true'
 
-REPOS_JSON_PATH = os.environ['FLASK_GITHUB_WEBHOOK_REPOS_JSON']
+# The repos.json file should be readable by the user running the Flask app,
+# and the absolute path should be given by this environment variable.
+#REPOS_JSON_PATH = os.environ['FLASK_GITHUB_WEBHOOK_REPOS_JSON']
+REPOS_JSON_PATH = '/home/sakemon/sakemon/repos.json'
 
 
 @app.route("/", methods=['GET', 'POST'])
@@ -37,7 +40,6 @@ def index():
     if request.method == 'GET':
         return 'OK'
     elif request.method == 'POST':
-	print request;
         # Store the IP address of the requester
         request_ip = ipaddress.ip_address(u'{0}'.format(request.remote_addr))
 
@@ -81,19 +83,11 @@ def index():
                 repo = repos.get('{owner}/{name}'.format(**repo_meta), None)
 
         if repo and repo.get('path', None):
-            # Check if POST request signature is valid
-            key = repo.get('key', None)
-            if key:
-                signature = request.headers.get('X-Hub-Signature').split('=')[1]
-                if type(key) == unicode:
-                    key = key.encode()
-                mac = hmac.new(key, msg=request.data, digestmod=sha1)
-                if not compare_digest(mac.hexdigest(), signature):
-                    abort(403)
-
             if repo.get('action', None):
                 for action in repo['action']:
+		    print "Action->", action
                     subp = subprocess.Popen(action, cwd=repo['path'])
+			
                     subp.wait()
         return 'OK'
 
