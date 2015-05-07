0cdaf6392964611e82a77c10fb00f633d2a7064c
mizjogit <git@mizjo.com>
2015-05-07 22:06:18 +1000

    webhook cleanup

--- webhook.py
+++ webhook.py
@@ -11,27 +11,9 @@ import hmac
 from hashlib import sha1
 from flask import Flask, request, abort
 
-"""
-Conditionally import ProxyFix from werkzeug if the USE_PROXYFIX environment
-variable is set to true.  If you intend to import this as a module in your own
-code, use os.environ to set the environment variable before importing this as a
-module.
-
-.. code:: python
-
-    os.environ['USE_PROXYFIX'] = 'true'
-    import flask-github-webhook-handler.index as handler
-
-"""
-if os.environ.get('USE_PROXYFIX', None) == 'true':
-    from werkzeug.contrib.fixers import ProxyFix
-
 app = Flask(__name__)
 app.debug = os.environ.get('DEBUG') == 'true'
 
-# The repos.json file should be readable by the user running the Flask app,
-# and the absolute path should be given by this environment variable.
-#REPOS_JSON_PATH = os.environ['FLASK_GITHUB_WEBHOOK_REPOS_JSON']
 REPOS_JSON_PATH = '/home/sakemon/sakemon/repos.json'
 
 
@@ -43,15 +25,8 @@ def index():
         # Store the IP address of the requester
         request_ip = ipaddress.ip_address(u'{0}'.format(request.remote_addr))
 
-        # If GHE_ADDRESS is specified, use it as the hook_blocks.
-        if os.environ.get('GHE_ADDRESS', None):
-            hook_blocks = [os.environ.get('GHE_ADDRESS')]
-        # Otherwise get the hook address blocks from the API.
-        else:
-            hook_blocks = requests.get('https://api.github.com/meta').json()[
-                'hooks']
+        hook_blocks = requests.get('https://api.github.com/meta').json()['hooks']
 
-        # Check if the POST request is from github.com or GHE
         for block in hook_blocks:
             if ipaddress.ip_address(request_ip) in ipaddress.ip_network(block):
                 break  # the remote_addr is within the network range of github.
@@ -87,7 +62,6 @@ def index():
                 for action in repo['action']:
 		    print "Action->", action
                     subp = subprocess.Popen(action, cwd=repo['path'])
-			
                     subp.wait()
         return 'OK'
 
